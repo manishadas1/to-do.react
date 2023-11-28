c7d08b88ee17017d6c386cf8d47747e18a3388f3
manishadas1 <manishadas.akrabhi@gmail.com>
2023-11-28 10:42:33 +0530

    to-do list completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+  const [editingTaskId, setEditingTaskId] = useState(null);
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleTaskInputChange = (event) => {
+    setTaskInput(event.target.value);
+  };
+
+  const handleAddEditTask = () => {
+    if (taskInput.trim() === '') return;
+
+    if (editingTaskId !== null) {
+      const updatedTasks = tasks.map((task) =>
+        task.id === editingTaskId ? { ...task, text: taskInput } : task
+      );
+      setTasks(updatedTasks);
+      setEditingTaskId(null);
+    } else {
+
+      const newTask = { id: Date.now(), text: taskInput, completed: false };
+      setTasks([...tasks, newTask]);
+    }
+
+    setTaskInput('');
+  };
+
+  const handleToggleComplete = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const handleEditTask = (taskId) => {
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    if (taskToEdit) {
+      setTaskInput(taskToEdit.text);
+      setEditingTaskId(taskId);
+    }
+  };
+
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={taskInput}
+          onChange={handleTaskInputChange}
+        />
+        <button onClick={handleAddEditTask}>
+          {editingTaskId !== null ? 'Edit Task' : 'Add Task'}
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : ''}>
+            {task.text}
+            <button onClick={() => handleToggleComplete(task.id)}>
+              {task.completed ? 'Undo' : 'Complete'}
+            </button>
+            <button onClick={() => handleEditTask(task.id)}>Edit</button>
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
